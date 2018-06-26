2dcaf94fbb2191a3fe355c41e910742561d473c3
Zonarius <zonarius90@gmail.com>
2018-06-26 21:18:29 +0200

    Use query parameter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get("/*", (req, res) => res.send("Hello whatever!"));
+app.get("/*", (req, res) => res.send(`Hello ${req.query.name}!`));
 
 app.listen(80);
